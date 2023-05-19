0842558d0d0f09b5be238ee082ec68cb5e29d6f5
Elyssa Tapawan <elyssatapawan@gmail.com>
2023-05-18 23:47:23 -0700

    Eighth Commit: Fixed Footer

--- src/Components/js/Footer.js
+++ src/Components/js/Footer.js
@@ -21,29 +21,45 @@ function Footer() {
     <Stack spacing={25} direction="row">
 
         <div>
-        <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 10}}>Developed By: </Typography>
-        <div className="justify-content-md-center" id="footer-line1"></div>
-        <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 25, marginTop: 50}}>Elyssa M. Tapawan </Typography>
-        <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 5}}>Email: </Typography>
-        <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 50}}>elyssatapawan@gmail.com </Typography>
+            <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 10}}>Developed By: </Typography>
+            <div className="justify-content-md-center" id="footer-line1"></div>
+            <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 25, marginTop: 50}}>Elyssa M. Tapawan </Typography>
+            <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 5}}>Email: </Typography>
+            <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 50}}>elyssatapawan@gmail.com </Typography>
         </div>
 
         <div>
-        <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 10}}>Supported By: </Typography>
-        <div className="justify-content-md-center" id="footer-line2"></div>
-        <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 25, marginTop: 50}}>RevAmp </Typography>
-        <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 5}}>Website: </Typography>
-        <Link className="link" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 50}}>https://www.superset.com/ </Link>
+            <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 10}}>Supported By: </Typography>
+            <div className="justify-content-md-center" id="footer-line2"></div>
+            <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 25, marginTop: 50}}>RevAmp </Typography>
+            <Typography sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 5}}>Website: </Typography>
+            <Link 
+            sx={{ flexGrow: 1 }} 
+            style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 50, color: 'black', textDecorationColor: 'black'}}>
+                https://www.superset.com/ 
+            </Link>
         </div>
 
         <div>
-        <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 10}}>Sources: </Typography>
-        <div className="justify-content-md-center" id="footer-line3"></div>
-        <Stack direction={'column'}>
-        <Link className="link" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 20, marginTop: 50}}>https://dog.ceo/api/breeds/image/random </Link>
-        <Link className="link" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 20}}>https://www.blobmaker.app/</Link>
-        <Link className="link" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 100}}>https://undraw.co/illustrations</Link>
-        </Stack>
+            <Typography variant="h6" sx={{ flexGrow: 1 }} style={{ fontFamily: "Inter", marginLeft: 50, marginRight: 50, marginBottom: 10}}>Sources: </Typography>
+            <div className="justify-content-md-center" id="footer-line3"></div>
+            <Stack direction={'column'}>
+                <Link 
+                sx={{ flexGrow: 1 }} 
+                style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 20, marginTop: 50, color: 'black', textDecorationColor: 'black'}}>
+                    https://dog.ceo/api/breeds/image/random 
+                </Link>
+                <Link 
+                sx={{ flexGrow: 1 }} 
+                style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 20, color: 'black', textDecorationColor: 'black'}}>
+                    https://www.blobmaker.app/
+                </Link>
+                <Link 
+                sx={{ flexGrow: 1 }} 
+                style={{ fontFamily: "Inter", marginLeft: 50, marginBottom: 100, color: 'black', textDecorationColor: 'black'}}>
+                    https://undraw.co/illustrations
+                </Link>
+            </Stack>
         </div>
 
     </Stack>
